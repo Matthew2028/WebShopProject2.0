dd9e23401f009c6e28328912b2a0bbbfd0a912ec
Matthew2028 <72547588+Matthew2028@users.noreply.github.com>
2021-02-25 18:04:36 +0100

    xbox page

---
+++ Scripts/everything.js
@@ -0,0 +1,22 @@
+
+var scrollTopBtn = document.querySelector(".scrollTopBtn")
+var rootElement = document.documentElement
+function handleScroll(){
+
+    var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
+    if((rootElement.scrollTop/scrollTotal)>0.80){
+        scrollTopBtn.classList.add("showBtn")
+    }else{
+        scrollTopBtn.classList.remove("showBtn")
+    }
+}
+
+function scrollToTop() {
+    // Scroll to top logic
+    rootElement.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+  }
+  scrollTopBtn.addEventListener("click",scrollToTop)
+  document.addEventListener("scroll",handleScroll)
